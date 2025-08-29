7d6d4582f972ac2e3825691e6c74372a421cd2b8
JustineEarlOrdaniza-UCM <ordanizajustineearl@gmail.com>
2025-08-29 22:06:54 +0800

    Add files via upload

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,13 @@
+public class FavoriteSong
+{
+  public static void main(String[] args)
+  {
+    System.out.println
+    (
+       "Say, Drake, I hear you like 'em young \n"+
+       "Certified Lover Boy? Certified pedophiles\n"+
+       "Why you trollin' like a bitch? Ain't you tired?\n"+
+       "Tryna strike a chord and it's probably A minor" 
+    );
+  }
+}
