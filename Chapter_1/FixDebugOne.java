7e9cdb636ffe987d437a2df2841cb0c7b4e7f04d
JustineEarlOrdaniza-UCM <ordanizajustineearl@gmail.com>
2025-08-31 09:28:00 +0800

    Add files via upload

---
+++ Chapter_1/FixDebugOne.java
@@ -0,0 +1,17 @@
+public class FixDebugOne
+{
+    /* This program displays a greeting */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
+
+/*
+List down the spotted bugs below:
+
+Bugs:
+1. Class name had a space - "FixDebug One" - "FixDebugOne
+2. Fixed main method - "Main(Sting args)" - "main(String[] args)"
+3. Typo in System - "Systm.out.println" - "System.out.println"
+*/
