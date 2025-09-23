e12b96436fd8bb9cc9af16e38a23934092f6be74
JustineEarlOrdaniza-UCM <ordanizajustineearl@gmail.com>
2025-09-23 18:27:53 +0800

    Add files via upload

---
+++ Chapter_4/DebugCircle.java
@@ -0,0 +1,30 @@
+public class DebugCircle
+   {
+    private int radius;
+    private int diameter;
+    private final double PI = 3.14159;
+    private double area;
+
+   
+    public DebugCircle(int r) {
+        radius = r;             
+        diameter = 2 * r;       
+        area = PI * r * r;      
+    }
+
+    
+    public int getRadius() { //remove the semicolon
+        return radius; //replace radiuss to radius      
+    }
+
+    
+    public int getDiameter() { // change void to int
+        return diameter;
+    }
+
+    
+    public double getArea() {
+        return area;
+    }
+}
+
