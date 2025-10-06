e7df16f9aea52bd2ba98130c42046b4f89233e31
JustineEarlOrdaniza-UCM <ordanizajustineearl@gmail.com>
2025-10-06 21:41:10 +0800

    Add files via upload

---
+++ Chapter_5/Payroll.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Payroll {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int empNum;
+        double rate;
+        int hours;
+
+        System.out.print("Enter employee number >> ");
+        empNum = input.nextInt();
+
+        System.out.print("How many hours did you work this week? ");
+        hours = input.nextInt();
+
+        System.out.print("What is your regular pay rate? ");
+        rate = input.nextDouble();
+
+        Employee emp = new Employee(empNum, rate);
+
+        double regularPay = emp.computeRegularPay(hours);
+        double overtimePay = emp.computeOvertimePay(hours);
+
+        System.out.println("Regular pay is " + regularPay);
+        System.out.println("Overtime pay is " + overtimePay);
+    }
+}
